8df0fc9ab00e03d015b114962c4f42557b5401f0
augustty1 <augustoguerra@dcc.ufmg.br>
2025-06-04 00:51:08 -0300

    kdtree basic implementation

--- app/cpp/include/kdcell.hpp
+++ app/cpp/include/kdcell.hpp
@@ -1,20 +1,26 @@
-
-/*
+/**
+* @file kdcell.hpp
 * @brief arquivo de cabeçalho para vertice da arvore k dimensional utilizada no trabalho pratico
-* autores : Augusto G.Lima, Cauã M.Pereira, Heitor G.Leite
-* histórico : 20250528 arquivo criado
+* @authors Augusto G.Lima, Cauã M.Pereira, Heitor G.Leite
+* @date 20250528 arquivo criado
 */
 
-#ifndef KDCELL_H
-#define KDCELL_H
+#ifndef KDCELL_HPP
+#define KDCELL_HPP
 
 #include<string>
 #include<iostream>
+
+/**
+* @brief definindo tipos abstratos de dados auxiliares, a saber
+* coordenadas, data e endereço para uma célula da kdtree
+*/
 
 class coordinates_t
 {
 public:
     double x,y;
+
     coordinates_t(double _x=0.0, double _y=0.0):x(_x),y(_y){}
 };
 
@@ -23,8 +29,9 @@ class date_t
 public:
     unsigned int day;
     unsigned int month;
-    // std::string month;
     unsigned int year;
+    // std::string month;
+
     date_t(unsigned int d=1, unsigned int m=1, unsigned int y=2000):day(d),month(m),year(y){}
 };
 
@@ -36,9 +43,14 @@ public:
     std::string neighborhood;
     unsigned int number;
     std::string complement;
-    address_t(const std::string& st="", cont std::string& stt="", const std::string& neig=0, unsigned int n="", const std::string & c=""):
+
+    address_t(const std::string& st="", const std::string& stt="", const std::string& neig="", unsigned int n=0, const std::string & c=""):
     street(st),street_type(stt),neighborhood(neig),number(n),complement(c){}
 };
+
+/*
+* @brief vertice da arvore de quadrantes
+*/
 
 class kdcell_t
 {
@@ -52,6 +64,7 @@ class kdcell_t
     bool permit; /// alvará
     date_t start_date;
     address_t address;
+
     kdcell_t(const coordinates_t& c, const std::string& n, const std::string& fn, bool p, const date_t& d, const address_t& a): 
     left(nullptr), right(nullptr), coordinates(c), name(n), fantasy_name(fn), permit(p), start_date(d), address(a) {}
 };
