542c017907a8a2c4f40e6b698d17fd368c25791f
cauamp <cauamp11@gmail.com>
2025-06-06 21:01:31 -0300

    refactor: ajusta formatação e organização do código nos arquivos de callbacks e componentes

--- app/components/map_component.py
+++ app/components/map_component.py
@@ -5,33 +5,33 @@
 #
 
 import dash_leaflet as dl
-from dash import html
 
-def create_map(center,zoom,bounds):
+
+def create_map(center, zoom, bounds):
     return dl.Map(
         id="bh-map",
         center=center,
         zoom=zoom,
         bounds=bounds,
         maxBounds=bounds,
-
         children=[
-        dl.TileLayer(),
-            dl.FeatureGroup([
-                dl.EditControl(
-                    
-                    id="edit-control",
-                    draw={"rectangle": True,
-                          "polygon": False, 
-                          "circle": False, 
-                          "polyline": False,
-                          "marker": False, 
-                          "circlemarker": False},
-                    
-                    edit={"edit": False, 
-                          "remove": True},
-                )
-            ]),
-            dl.LayerGroup(id="layer-point-group")
-        ]
+            dl.TileLayer(),
+            dl.FeatureGroup(
+                [
+                    dl.EditControl(
+                        id="edit-control",
+                        draw={
+                            "rectangle": True,
+                            "polygon": False,
+                            "circle": False,
+                            "polyline": False,
+                            "marker": False,
+                            "circlemarker": False,
+                        },
+                        edit={"edit": False, "remove": True},
+                    )
+                ]
+            ),
+            dl.LayerGroup(id="layer-point-group"),
+        ],
     )
--- app/index.py
+++ app/index.py
@@ -1,21 +1,25 @@
-from dash import Dash,html
+from dash import html
 from app import app
 from components.map_component import create_map
 from callbacks.map_callbacks import register_callbacks
 from utils.data_loader import load_data
+import os
 
-center_bh=(-19.912998, -43.940933)
-zoom_bh=12
-bounds_bh=[[-20.048, -44.079], [-19.784, -43.800]]
-dataframe=load_data()
+center_bh = (-19.912998, -43.940933)
+zoom_bh = 12
+bounds_bh = [[-20.048, -44.079], [-19.784, -43.800]]
+dataframe = load_data()
 
-app.layout=html.Div([
-    html.H1("Belo Horizonte Orthogonal Range Search KD-Tree"),
-    create_map(center_bh,zoom_bh,bounds_bh),
-    html.Div(id="points-info")
-])
+app.layout = html.Div(
+    [
+        html.H1("Belo Horizonte Orthogonal Range Search KD-Tree"),
+        create_map(center_bh, zoom_bh, bounds_bh),
+        html.Div(id="points-info"),
+    ]
+)
 
 register_callbacks(app, dataframe)
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    debug = os.environ.get("DEBUG", "False").lower() in ("true", "1", "yes")
+    app.run(debug=debug)
